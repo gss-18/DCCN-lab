9ff6785ba03f5a02165311e4641cd99f0c84fb8b
gss-18 <gsstrivikram18@gmail.com>
2021-08-25 22:58:39 +0530

    Update MultiDimension_parity.py

--- MultiDimension_parity.py
+++ MultiDimension_parity.py
@@ -12,7 +12,6 @@ ver=list(np.zeros(len(mat),dtype=int))
 for i in range(len(mat)):
     mat[i]=mat[i].split(' ')
 
-
 for i in mat:
     x=dict(Counter(i))
     if(x["1"]%2==0):
@@ -34,10 +33,45 @@ for i in range(len(ver)):
 
 print('Vertical Parity',' '.join(str(i) for i in ver)) 
 
+print("\n \nRecieved Data: ")
+b=int(input("Enter rows: "))
+matb=[]
+horb=[]
+verb=[]
+for i in range(b):
+    matb.append(input('Enter row data: '))
+verb=list(np.zeros(len(matb),dtype=int))
+
+for i in range(len(matb)):
+    matb[i]=matb[i].split(' ')
 
 
+for i in matb:
+    x=dict(Counter(i))
+    if(x["1"]%2==0):
+        horb.append('0')
+    else:
+        horb.append('1')
 
+print('Horizontal Parity: ',' '.join(horb)) 
 
+for i in range(len(matb)):
+    for j in range(len(matb[i])):
+        verb[i]+=int(matb[i][j])
+
+for i in range(len(verb)):
+    if(verb[i]%2==0):
+        verb[i]=0
+    else: 
+        verb[i]=1
+print('Vertical Parity: ',' '.join(str(i) for i in verb)) 
+
+if(hor==horb and ver==verb):
+    print('No error')
+    print("Corrected Output: ",mat)
+else:
+    print('Error exist')
+    print("Corrected Output: ",mat)
 
 
 """ 1 1 0  0
